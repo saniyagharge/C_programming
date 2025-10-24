01c6cee6e7bedcf7968e2ecb87e6e6e0f2e3a016
saniyagharge <ghargesaniya1407@gmail.com>
2025-10-24 15:18:06 +0530

    Add files via upload
    Added Assignment_2 programs

---
+++ Assignments/Assignment_2/program_1.c
@@ -0,0 +1,50 @@
+// Accept one number from user and print that number of * on screen.
+
+////////////////////////////////////////////////////////////
+//
+// Required Header Files
+//
+////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+////////////////////////////////////////////////////////////
+//
+//  Function Name : Display
+//  Description :   Used to print * on screen.
+//  Input :         Integer
+//  Output :        *
+//  Author :        Saniya Gharge
+//  Date :          24/10/2025
+//
+////////////////////////////////////////////////////////////
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    while (iCnt < iNo)
+    {
+        printf("*");
+
+        iCnt++;
+    }
+}
+
+////////////////////////////////////////////////////////////
+//
+// Entry point function for the Application.
+//
+////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number :");
+
+    scanf("%d", &iValue);
+
+    Display(iValue);
+    return 0;
+}
